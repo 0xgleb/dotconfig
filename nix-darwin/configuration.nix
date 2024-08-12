3e68191e8569abd39b30a95580e932ed1d751de6
0xgleb <gleb.dianov@protonmail.com>
2024-08-12 16:31:02 +0100

    home manager

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,39 +1,51 @@
 self@{ pkgs, ... }:
 {
+  users.users."0xgleb" = {
+    home = "/Users/0xgleb";
+    shell = pkgs.zsh;
+  };
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+  programs.direnv.enable = true;
+  programs.vim.enable = true;
+  programs.vim.enableSensible = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     bat
     emacs
-    emacsPackages.doom
-    karabiner-elements
     magic-wormhole
-    direnv
     openssl
+    zellij
+    telegram-desktop
+    _1password
+    htop
+    fzf
+    obsidian
     iterm2
-    vim
+    tldr
     git
     gh
+    jq
   ];
-  
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-  
+  nix.package = pkgs.nix;
+
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
-  
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
-  
+
   # Set Git commit hash for darwin-version.
   system.configurationRevision = self.rev or self.dirtyRev or null;
-  
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
-  
+
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "x86_64-darwin";
+  nixpkgs.config.allowUnfree = true;
 }
--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,10 +2,10 @@
   description = "DarWwWin";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -14,7 +14,18 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake ~/.config/nix-darwin#darwwwin
     darwinConfigurations.darwwwin = nix-darwin.lib.darwinSystem {
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users."0xgleb" = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
