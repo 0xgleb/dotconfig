ac215063ee89e26ab1b824b0a0d4ae12a37954ea
0xgleb <gleb.dianov@protonmail.com>
2026-03-26 14:24:02 +0700

    feat: codex

--- home.nix
+++ home.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  lib,
+  inputs,
   config,
   ...
 }:
@@ -18,20 +18,32 @@ in
     username = "0xgleb";
     stateVersion = "24.05";
 
-    packages = with pkgs; [
-      cargo-watch
-      ghostty-bin
-    ];
+    packages =
+      let
+        unstable = import inputs.nixpkgs-unstable {
+          system = pkgs.stdenv.hostPlatform.system;
+          config.allowUnfree = true;
+        };
+
+      in
+      with pkgs;
+      [
+        cargo-watch
+        ghostty-bin
+        unstable.graphite-cli
+
+        unstable.codex
+        unstable.claude-code
+      ];
 
     shell.enableNushellIntegration = true;
     file."${nuConfigDir}/scripts".source = ./nushell/scripts;
     file.".config/nushell/config.nu".source =
-      config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/Library/Application Support/nushell/config.nu";
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Library/Application Support/nushell/config.nu";
     file.".config/nushell/env.nu".source =
-      config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/Library/Application Support/nushell/env.nu";
-    file."Library/Application Support/com.mitchellh.ghostty/config.ghostty".source = ./ghostty/config.ghostty;
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Library/Application Support/nushell/env.nu";
+    file."Library/Application Support/com.mitchellh.ghostty/config.ghostty".source =
+      ./ghostty/config.ghostty;
   };
 
   # NOTE: this shit doesn't clean up after itself if you enable/disable it
